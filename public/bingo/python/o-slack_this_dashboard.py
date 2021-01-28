fe7dc5e60639bb61501120199f3e8036893985ef
Chris <chris.hastie@interworks.co.uk>
2021-01-28 10:01:18 +0000

    O - Slack me this dashboard - Ready for testing once app is approved

--- public/bingo/python/o-slack_this_dashboard.py
+++ public/bingo/python/o-slack_this_dashboard.py
@@ -4,6 +4,7 @@
 # ## This script creates a function which creates users in Tableau server
 # ## by leveraging the Tableau REST API and TableauServerClient Python package
 
+
 def retrieve_dashboard_image(
     slack_oath_token = '',
     tableau_token_value = '',
@@ -12,13 +13,6 @@ def retrieve_dashboard_image(
 ) :
     import tableauserverclient as tsc
     import getpass
-    import requests
-    import json
-    import base64
-
-    # Request user password if not provided already
-    if slack_oath_token == '' :
-        slack_oath_token = getpass.getpass('Slack OAuth token:')
 
     # Request user password if not provided already
     if tableau_token_value == '' :
@@ -30,38 +24,87 @@ def retrieve_dashboard_image(
     with server.auth.sign_in(tableau_auth):
         all_view_items, pagination_item = server.views.get()
         view_item = all_view_items[0]
-        server.views.populate_csv(view_item)
+
         server.views.populate_image(view_item)
 
-        with open('./view_image.png', 'wb') as f:
-            f.write(view_item.image)
+        return view_item.image
 
-        print(b''.join(view_item.csv).decode("utf-8"))
+def post_image_to_slack(
+    slack_oath_token = '',
+    channels = '#tmp-slack-webhook-testing-playground',
+    file_name = './view_image.png',
+    comment = 'Here\'s my test file :smile:'
+) :
+    # Import WebClient from Python SDK (github.com/slackapi/python-slack-sdk)
+    from slack_sdk import WebClient
+    from slack_sdk.errors import SlackApiError
 
-    url = "https://slack.com/api/chat.postMessage"
+    # Request user password if not provided already
+    if slack_oath_token == '' :
+    slack_oath_token = getpass.getpass('Slack OAuth token:')
 
-    payload= json.dumps({"channel": "#tmp-slack-webhook-testing-playground", "text": "Testing"})
-    headers = {
-      'Authorization': 'Bearer {0}'.format(slack_oath_token),
-      'Content-Type': 'application/json'
-    }
+    # WebClient insantiates a client that can call API methods
+    # When using Bolt, you can use either `app.client` or the `client` passed to listeners.
+    client = WebClient(token=slack_oath_token)
+    # The name of the file you're going to upload
 
-    response = requests.request("POST", url, headers=headers, data=payload)
+    try:
+        # Call the files.upload method using the WebClient
+        # Uploading files requires the `files:write` scope
+        result = client.files_upload(
+            channels=channels,
+            initial_comment=comment,
+            file=file_name
+        )
+        # Log the result
+        logger.info(result)
 
-    ##############################
+    except SlackApiError as e:
+        logger.error("Error uploading file: {}".format(e))
 
-    url = "https://slack.com/api/files.upload"
+    # url = "https://slack.com/api/chat.postMessage"
+    #
+    # payload= json.dumps({"channel": "#tmp-slack-webhook-testing-playground", "text": "Testing"})
+    # headers = {
+    #   'Authorization': 'Bearer {0}'.format(slack_oath_token),
+    #   'Content-Type': 'application/json'
+    # }
+    #
+    # response = requests.request("POST", url, headers=headers, data=payload)
+    #
+    # ##############################
+    #
+    # url = "https://slack.com/api/files.upload"
+    #
+    # payload= {"channels": "#tmp-slack-webhook-testing-playground", "title": "Testing", "file" : view_item.image, "filetype" : "png"}
+    # headers = {
+    #   'Authorization': 'Bearer {0}'.format(slack_oath_token),
+    #   'Content-Type': 'multipart/form-data'
+    # }
+    #
+    # response = requests.request("POST", url, headers=headers, data=payload)
+    #
+    # response.text
 
-    payload= {"channels": "#tmp-slack-webhook-testing-playground", "title": "Testing", "file" : view_item.image, "filetype" : "png"}
-    headers = {
-      'Authorization': 'Bearer {0}'.format(slack_oath_token),
-      'Content-Type': 'multipart/form-data'
-    }
+def retrieve_dashboard_image_and_post_to_slack(
+    slack_oath_token = '',
+    tableau_token_value = '',
+    tableau_token_name = 'datadev',
+    tableau_content_url = 'chrishastieiwdev598367'
+) :
+    import tempfile
 
-    response = requests.request("POST", url, headers=headers, data=payload)
+    view_image = retrieve_dashboard_image(slack_oath_token, tableau_token_value, tableau_token_name, tableau_content_url)
+
+    # create a temporary file using a context manager
+    with tempfile.TemporaryFile(suffix='.png') as fp:
+        fp.write(view_image)
+
+        post_image_to_slack(slack_oath_token, channels = '#tmp-slack-webhook-testing-playground', file_name = fp.name, comment = 'Here\'s my test file :smile:')
 
 
-retrieve_dashboard_image(
+retrieve_dashboard_image_and_post_to_slack(
+    slack_oath_token = '',
     tableau_token_value = '',
     tableau_token_name = 'datadev',
     tableau_content_url = 'chrishastieiwdev598367'
